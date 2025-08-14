3ce735d9a2ccaf47926fd84a302b394a57b2c923
Tcohneyn <chenminami@outlook.com>
2025-08-14 22:04:17 +0800

    初始化项目

---
+++ Source/CommonUIProject/CommonUIProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CommonUIProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CommonUIProject, "CommonUIProject" );
